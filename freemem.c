c00e3521c8fa25373acc321375b8abc9c47bdf0a
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-30 10:05:17 +0530

    completed basic code

--- freemem.c
+++ freemem.c
@@ -3,16 +3,28 @@
 
 // import all the serious stuff
 #include<stdio.h>
+#include<stdlib.h>
 
 // the main function
 int main()
 {
 	// open the drop_caches file
+	file_pointer = fopen("/proc/sys/vm/drop_caches", "w");
+
+	// if NULL pointer is returned, exit
+	if(file_pointer == NULL)
+	{
+		printf("error: no permissions to write to file");
+		exit(1);
+	}
 	
-	// write 1, ie. remove pages
+	// write to the file
+	fprintf(file_pointer, "1");
 	
 	// close the file
+	fclose(file_pointer);
 	
 	// exit
+	printf("memory cleared!\n");
 	return 0;
 }
