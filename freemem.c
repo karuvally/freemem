d04d7f33c56e44e5fc8b53e46db0afccc8dc6de7
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-30 09:57:17 +0530

    added freemem.c

---
+++ freemem.c
@@ -0,0 +1,18 @@
+// freemem, alpha release
+// Copyright 2019, Aswin Babu Karuvally
+
+// import all the serious stuff
+#include<stdio.h>
+
+// the main function
+int main()
+{
+	// open the drop_caches file
+	
+	// write 1, ie. remove pages
+	
+	// close the file
+	
+	// exit
+	return 0;
+}
