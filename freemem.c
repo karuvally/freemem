890f2f8c50fb9dd539b62d0b4345cfd44b4c711f
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-03-30 10:08:30 +0530

    bugfix: missing newline in error msg

--- freemem.c
+++ freemem.c
@@ -9,12 +9,13 @@
 int main()
 {
 	// open the drop_caches file
+	FILE *file_pointer;
 	file_pointer = fopen("/proc/sys/vm/drop_caches", "w");
 
 	// if NULL pointer is returned, exit
 	if(file_pointer == NULL)
 	{
-		printf("error: no permissions to write to file");
+		printf("error: no permissions to write to file\n");
 		exit(1);
 	}
 	
